dee6b6a9939e357b19c00f38082d553a53dfbf0f
Caroline Graebel <cagr4985@bht-berlin.de>
2024-07-01 22:42:56 +0200

    First look at connections between happiness score and other variables
    Top level look at connection between happiness score and the other variables. I also used a RandomForest model because the R package also comes with a feature importance function that I wanted to try.

---
+++ R scripts/FirstLook.R
@@ -0,0 +1,81 @@
+# needed libraries
+library(randomForest)
+library(tidyverse)
+
+# load data
+whr <- read.csv("Data/WHR_AllYears.csv")
+# delete X
+whr$X <- NULL
+
+summary(whr)
+str(whr)
+
+table(whr$country)
+# some countries don't appear 9 times / "each year" in the data
+
+table(whr$region)
+
+table(whr$year)
+# over time there are less countries present in the data
+
+# given happiness score is the most important feature we want to look at:
+# how does it interact with the other variables?
+
+attach(whr)
+
+# country not plotted because too smallgrained
+
+boxplot(happiness_score ~ region, las = 2, xlab = "")
+# Western Europe and North America / ANZ are the happiest in the world
+# Sub Saharan Africa is lowest
+# North Africa has a big variance
+
+plot(happiness_score, gdp_per_capita)
+# higher gdp per capita means higher happiness score
+
+plot(happiness_score, social_support)
+# higher social support means higher happiness
+
+plot(happiness_score, healthy_life_expectancy)
+# higher (better?) healthy life expectancy means higher happiness
+
+plot(happiness_score, freedom_to_make_life_choices)
+# more freedom for life choices means higher happiness
+
+plot(happiness_score, generosity)
+# rather mixed, no real connection
+
+plot(happiness_score, perceptions_of_corruption)
+# generally there's a lot of values that are low on perception of corruption
+# for the high happy values there's a lot of strong perception of corruption
+# which is weird
+
+boxplot(happiness_score ~ year)
+# for the median values there is an upwards trend over the years
+# quite a big variance in 2020 and 2023
+
+detach(whr)
+
+# find out which features influence the happiness score the most when training
+# a random forest model
+# just out of curiosity
+
+# do copy of data set without NAs
+whr_noNA <- na.omit(whr)
+
+# train random forest
+set.seed(1)
+rf.whr <- randomForest(happiness_score ~ ., data = whr_noNA, mtry = 6, ntRee = 100,
+                       importance = TRUE)
+
+# plot importance of variables
+varImpPlot(rf.whr)
+# %IncMSE indicates the increase of the mean squared error when the variable
+# is randomly permuted
+# IncNodePurity is a measure of how much the model error increases when a particular
+# variable is randomly permuted or shuffled
+
+# top variables change depending on the seed and the mtry variable set
+# however the bottom with year, social_support and generosity is stable over
+# each iteration
+# the first 4 are also pretty stable, even if they can switch places
